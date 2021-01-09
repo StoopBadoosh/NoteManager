fdcd8ecceef1e77061c2940caa1e529127782674
Anish Nambirajan <anish.nambi@gmail.com>
2021-01-08 19:16:51 -0800

    integrated flask app with mongo

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
-app=Flask('Anish')
+from flask_pymongo import PyMongo
 
+#here we are getting the connection string
+file=open(".env","r")
+connection_string=file.read()
+connection_string=connection_string.strip()
+file.close()
+
+#here we are creating our flask app
+app=Flask('Anish')
+app.config['MONGO_URI']=connection_string
+
+#here we are combining our flask object with mongodb
+mongo=PyMongo(app)
 @app.route('/')
 def landing_page():
     return render_template('index.html')
